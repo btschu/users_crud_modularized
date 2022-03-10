85ee9b17fcbd34a5d19ad6f721611d4dfe19e174
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-03-09 20:56:40 -0700

    cleaned up and added full name function

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,13 +2,20 @@ from flask import render_template,redirect,request
 from flask_app import app
 from flask_app.models.user import User
 
+# VIEW ALL USERS
+
 @app.route('/')
 def index():
     return redirect('/users')
 
 @app.route('/users')
 def users():
-    return render_template("read_all.html", users = User.get_all())
+    context = {
+        'users' : User.get_all()
+    }
+    return render_template("read_all.html", **context)
+
+# Create a NEW USER
 
 @app.route('/users/new')
 def new():
@@ -19,24 +26,36 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+# Update User
+
 @app.route('/user/edit/<int:id>')
 def edit(id):
     data ={
         "id":id
     }
-    return render_template("edit.html", user = User.get_one(data))
+    context = {
+        'user' : User.get_one(data)
+    }
+    return render_template("edit.html", **context)
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+# Show one user
 
 @app.route('/user/show/<int:id>')
 def show(id):
     data ={
         "id":id
     }
-    return render_template("read_one.html", user = User.get_one(data))
+    context = {
+        'user' : User.get_one(data)
+    }
+    return render_template("read_one.html", **context)
 
-@app.route('/user/update',methods=['POST'])
-def update():
-    User.update(request.form)
-    return redirect('/users')
+# delete user
 
 @app.route('/user/delete/<int:id>')
 def delete(id):
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,5 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
+db = "users_schema"
+
 class User:
 
     def __init__( self , data ):
@@ -10,32 +12,46 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    def full_name(self):
+        fullname = f"{self.first_name} {self.last_name}"
+        return fullname
+
     @classmethod
-    def get_one(cls,data):
-        query  = "SELECT * FROM users WHERE id = %(id)s";
-        result = connectToMySQL('users_schema').query_db(query,data)
-        return cls(result[0])
+    def save(cls, data ):
+        query = """
+        INSERT INTO users (first_name, last_name, email, created_at, updated_at)
+        VALUES (%(fname)s, %(lname)s, %(email)s, NOW(), NOW());"""
+        return connectToMySQL(db).query_db( query, data )
 
     @classmethod
     def update(cls,data):
-        query = "UPDATE users SET first_name=%(fname)s,last_name=%(lname)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
-        return connectToMySQL('users_schema').query_db(query,data)
+        query = """
+        UPDATE users
+        SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = NOW()
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query,data)
 
     @classmethod
-    def delete(cls,data):
-        query  = "DELETE FROM users WHERE id = %(id)s;"
-        return connectToMySQL('users_schema').query_db(query,data)
+    def get_one(cls,data):
+        query  = """
+        SELECT * FROM users
+        WHERE id = %(id)s;"""
+        result = connectToMySQL(db).query_db(query,data)
+        return cls(result[0])
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
+        results = connectToMySQL(db).query_db(query)
         users = []
         for user in results:
             users.append( cls(user) )
         return users
 
     @classmethod
-    def save(cls, data ):
-        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
-        return connectToMySQL('users_schema').query_db( query, data )
+    def delete(cls,data):
+        query  = """
+        DELETE FROM users
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query,data)
+
